328dbe66cf3495299750128fbfafcb842a420e69
noel <noelati@gmail.com>
2022-07-18 22:41:34 -0400

    nada

--- 1.js
+++ 1.js
@@ -1,8 +1,8 @@
 // 1. Rellena un array con los números del 1 al 10. Muéstralo por la consola.
 
 let array = [];
-
 for (let i = 1; i < 11; i++) {
   array.push(i);
 }
-// console.log(array);
+console.log(array);
+
